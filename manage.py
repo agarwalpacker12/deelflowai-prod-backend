495b469c9f627e81f4de9e79b97a7a80bf81aae3
dhiman <11156539+titoforu@users.noreply.github.com>
2025-09-17 03:59:32 +0530

    fixed

--- manage.py
+++ manage.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
-import django
-
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deelflowAI.settings')
-    django.setup()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Make sure it's installed in your virtual environment "
+            "and that the environment is activated. Check PYTHONPATH and DJANGO_SETTINGS_MODULE."
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     main()
+
+
